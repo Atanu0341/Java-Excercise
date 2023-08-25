8ee77efe7a38ede3ca2d50395a4ab37e0c4f3981
Atanu Majumder <atanumajumder2004@gmail.com>
2023-08-25 12:08:15 +0530

    question 8

---
+++ 01-first-java/StringPalindrome_8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StringPalindrome_8 {
+
+    public static void main(String[] args) {
+        String a,b = "";
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        a = s.nextLine();
+        int n = a.length();
+        for(int i = n-1;i>=0;i--){
+            b = b + a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b)){
+            System.out.println(a+ " is a palindrome");
+        }
+        else{
+            System.out.println(a+ " is not a palindrome");
+        }
+    }
+}
